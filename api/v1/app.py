68c6a2628dc6278e22bc06f4c7ebb083d2ef4fbb
NyatiZw <appsmaid19@gmail.com>
2023-08-01 07:32:54 +0000

    starting API

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,24 +9,27 @@ from api.v1.views import app_views
 import os
 from flask_cors import CORS, cross_origin
 
-#Flask Application Variable
+"""Flask Application Variable"""
 app = Flask(__name__)
 
 app.url_map.strict_slashes = False
 
 
-# Declare a method to handle teardown
+""" Declare a method to handle teardown"""
+
+
 def teardown(exception):
     storage.close()
 
-# Flask server environment setup
+
+""" Flask server environment setup"""
 host = os.getenv('HBNB_API_HOST', '0.0.0.0')
 port = os.getenv('HBNB_API_PORT', 5000)
 
-#Cross-Origin Resource Sharing
+"""Cross-Origin Resource Sharing"""
 cors = CORS(app, resources={r'/*': {'origins': host}})
 
-#Register blueprint app_views for Flask instance
+"""Register blueprint app_views for Flask instance"""
 app.register_blueprint(app_views)
 
 
