4baedc696f7a4e0330691d6a267e1f9a7ef8536d
NyatiZw <appsmaid19@gmail.com>
2023-08-01 03:57:36 +0200

    init files

---
+++ api/v1/app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+API to return status
+"""
+
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+import os
+
+
+# Flask Application Variable
+app = Flask(__name__)
+
+
+app.url_map.strict_slashes = False
+
+# Register blueprint app_views for Flask instance
+app.register_blueprint(app_views)
+
+# Declare a method to handle teardown
+def teardown(exception):
+    storage.close()
+
+# Flask server environment setup
+host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+port = os.getenv('HBNB_API_PORT', 5000)
+
+
+if __name__ == "__main__":
+    """
+    Main Flask App
+    """
+    app.run(host=host, port=port)
