517034924e4682a9750b52211c27b7e4922283d9
NyatiZw <appsmaid19@gmail.com>
2023-08-02 03:16:06 +0000

    corrrections

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,9 +1,25 @@
 from flask import Blueprint
+
+# Create a Blueprint instance
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+# Wildcard import of everything in the package
 from api.v1.views.index import *
+
+# Wildcard import of everything in the package
 from api.v1.views.states import *
+
+# Wildcard import of everything in the package
 from api.v1.views.cities import *
+
+# Wildcard import of everything in the package
 from api.v1.views.amenities import *
+
+# Wildcard import of everything in the package
 from api.v1.views.users import *
+
+# Wildcard import of everything in the package
 from api.v1.views.places import *
+
+# Wildcard import of everything in the package
 from api.v1.views.places_reviews import *
