aa601014e4c638d018c70012dd51aa85a4b84e71
NyatiZw <appsmaid19@gmail.com>
2023-08-02 00:04:10 +0000

    adding amenities api

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,8 +1,7 @@
 from flask import Blueprint
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 from api.v1.views.index import *
-from . import states, cities
-from . import amenities
+from . import states, cities, amenities
 from . import users
 from . import places
 from . import places_reviews
---
+++ api/v1/views/amenities.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""
+Amenity Api handles
+"""
+
+from flask import Blueprint, request, jsonify, abort
+from models.amenity import Amenity
+
+
+amenities_bp = Blueprint('amenities', __name__, url_prefix='/api/v1/amenities')
+
+
+@amenities_bp.route('', methods=['GET'])
+def get_all_amenities():
+    amenities = Amenity.query.all()
+    return jsonify([amenity.to_dict() for amenity in amenities]), 200
+
+@amenities_bp.rout('/<int:amenity_id>', methods=['GET'])
+def get_amenity(amenity_id):
+    amenity = Amenity.query.get(amenity_id)
+    if not amenity:
+        abort(404)
+    return jsonify(amenity.to_dict()), 200
+
+@amenities_bp.route('/<int:amenity_id>', methods=['DELETE'])
+def delete_amenity(amenity_id):
+    amenity = Amenity.query.get(amenity_id)
+    if not amenity:
+        abort(404)
+    amenity.delete()
+    return jsonify({}), 200
+
+
+@amenities_bp.route('', methods=['POST'])
+def create_amenity():
+    data = request.get_json()
+    if not data:
+        abort(400, 'Not a JSON')
+    if 'name' not in data:
+        abort(400, 'Missing name')
+    amenity = Amenity(name=data['name'])
+    amenity.save()
+    return jsonify(amenity.to_dict()), 201
+
+@amenities_bp.route('/<int:amenity_id>', methods=['PUT'])
+def update_amenity(amenity_id):
+    amenity = Amenity.query.get(amenity_id)
+    if not amenity:
+        abort(404)
+    data = request.get_json()
+    if not data:
+        abort(400, 'Not a JSON')
+    ignored_keys = ['id', 'created_at', 'updated_at']
+    for key, value in data.items():
+        if key not in ignored_keys:
+            setattr(amenity, key, value)
+    amenity.save()
+    return jsonify(amenity.to_dict()), 200
